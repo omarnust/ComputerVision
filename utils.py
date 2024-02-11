0ebf262cfb073eb9aba87195e96d8a8760336fe1
Lamis Elsayed <lamisahmed2002@gmail.com>
2024-02-11 23:35:07 +0400

    added comments

--- utils.py
+++ utils.py
@@ -11,10 +11,10 @@ def getImage(url, mode = cv2.IMREAD_COLOR):
     image = cv2.imdecode(image_array, mode)    
     return image
     
-def gaussian_noise(image, mean=0, sigma=25):
+def gaussian_noise(image, mean=0, sigma=25):            # most of the time it'll be centered around 0 (you can play with the mean & sigma values to see how it changes)
     row, col = image.shape
     gauss = np.random.normal(mean, sigma, (row, col))
-    noisy = np.clip(image + gauss, 0, 255)
+    noisy = np.clip(image + gauss, 0, 255)              # add the noise to the image and clip it such that no pixel exceeds 255 or is below 0
     return noisy.astype(np.uint8)
 
 def sp_noise(image, prob):
@@ -23,18 +23,20 @@ def sp_noise(image, prob):
     prob: Probability of the noise
     '''
     output = image.copy()
-    if len(image.shape) == 2:
+    if len(image.shape) == 2:                       # grayscale -- (height, width)
         black = 0
         white = 255            
-    else:
-        colorspace = image.shape[2]
+    else:                                           # color     -- (height, width, no_channels)
+        colorspace = image.shape[2] # no_channels
         if colorspace == 3:  # RGB
             black = np.array([0, 0, 0], dtype='uint8')
             white = np.array([255, 255, 255], dtype='uint8')
-        else:  # RGBA
+
+        else:               # RGBA (A = alpha --> transparency/opacity)
             black = np.array([0, 0, 0, 255], dtype='uint8')
             white = np.array([255, 255, 255, 255], dtype='uint8')
-    probs = np.random.random(output.shape[:2])
+
+    probs = np.random.random(output.shape[:2])      # generate random probabilities for each pixel
     output[probs < (prob / 2)] = black
     output[probs > 1 - (prob / 2)] = white
     return output
@@ -52,4 +54,6 @@ def show_collage(figsize, nrows, ncols, **kwargs):
         plt.title(kwargs['titles'][i])
         plt.axis('off')
     
+    # plt will automatically scale in and give a valid image, even if you give it pixel values within -255 and 0 range
+    # but OpenCV only works within a valid range (i.e., 0-255, or 0-1)
             
